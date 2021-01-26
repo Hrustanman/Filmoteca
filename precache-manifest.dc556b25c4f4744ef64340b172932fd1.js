4cf3328aa2e778b86dcb2638a15c715029c20ca4
hrust <hrust@DESKTOP-HHVK3MP>
2021-01-26 18:15:03 +0200

    Updates

---
+++ precache-manifest.dc556b25c4f4744ef64340b172932fd1.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28f66748412ad03c61b342a5b6d5b373",
+    "url": "/Filmoteca/index.html"
+  },
+  {
+    "revision": "c10069c9fe73a9ef7c68",
+    "url": "/Filmoteca/static/css/main.69881c93.chunk.css"
+  },
+  {
+    "revision": "1dc3efec947a027a2dd1",
+    "url": "/Filmoteca/static/js/2.f0648747.chunk.js"
+  },
+  {
+    "revision": "c10069c9fe73a9ef7c68",
+    "url": "/Filmoteca/static/js/main.6b1b5d7e.chunk.js"
+  },
+  {
+    "revision": "0c82f8f3e4715ba2ee92",
+    "url": "/Filmoteca/static/js/runtime~main.48fc8c5a.js"
+  },
+  {
+    "revision": "9c57862f94ba4d6179b8cf252aeb89d8",
+    "url": "/Filmoteca/static/media/apocalypto.9c57862f.jpg"
+  },
+  {
+    "revision": "f81f8497423abf4d50dfe7707e16e504",
+    "url": "/Filmoteca/static/media/atlantis.f81f8497.jpg"
+  },
+  {
+    "revision": "7c06c467999c1bb01311cd9bf62c2676",
+    "url": "/Filmoteca/static/media/beautifulMind.7c06c467.jpg"
+  },
+  {
+    "revision": "ed60b0c02d9abaa59c4e474236ecfe16",
+    "url": "/Filmoteca/static/media/envelope.ed60b0c0.png"
+  },
+  {
+    "revision": "8f5ce27564945d2c9a10ef827549a78c",
+    "url": "/Filmoteca/static/media/facebook.8f5ce275.png"
+  },
+  {
+    "revision": "53a046a1880602a10495074247fcc4b4",
+    "url": "/Filmoteca/static/media/felix-movie.53a046a1.jpg"
+  },
+  {
+    "revision": "1cdba73cd798bd8327088b9b0b2a0348",
+    "url": "/Filmoteca/static/media/harry.1cdba73c.jpg"
+  },
+  {
+    "revision": "14f7b6753cb58692c0da571c41b33210",
+    "url": "/Filmoteca/static/media/in.14f7b675.svg"
+  },
+  {
+    "revision": "7a4cee66e51b532b18b9673d01c774c0",
+    "url": "/Filmoteca/static/media/interstellar.7a4cee66.jpg"
+  },
+  {
+    "revision": "361dddc5b92e2a25044ecd26a52e8705",
+    "url": "/Filmoteca/static/media/intoTheWild.361dddc5.jpg"
+  },
+  {
+    "revision": "3b359d91b5c8a7e68302bcf44347a4be",
+    "url": "/Filmoteca/static/media/jungle.3b359d91.jpg"
+  },
+  {
+    "revision": "f373fc6dab8943ec1077222e775abb64",
+    "url": "/Filmoteca/static/media/law-abiding.f373fc6d.jpg"
+  },
+  {
+    "revision": "c21f4d79e2cc05c37de0f74bd8f7886a",
+    "url": "/Filmoteca/static/media/mapflag.c21f4d79.png"
+  },
+  {
+    "revision": "43dd7c3a1e4ecbd9348e2a3e0683061c",
+    "url": "/Filmoteca/static/media/rampage.43dd7c3a.jpg"
+  },
+  {
+    "revision": "7871e9784acc00dc2b90f2e4932f581b",
+    "url": "/Filmoteca/static/media/sloane.7871e978.jpg"
+  },
+  {
+    "revision": "31bd0dba864497b7a22d9e25c87a67d3",
+    "url": "/Filmoteca/static/media/sniper.31bd0dba.jpg"
+  },
+  {
+    "revision": "0251d8ee95aa6d1f3400faa3b46b4bcf",
+    "url": "/Filmoteca/static/media/twitter.0251d8ee.png"
+  },
+  {
+    "revision": "fec46a206b9d854f354aa504ff8d593e",
+    "url": "/Filmoteca/static/media/unknown.fec46a20.jpg"
+  },
+  {
+    "revision": "a6372485318244c18a497ae9a9b62467",
+    "url": "/Filmoteca/static/media/yt.a6372485.svg"
+  },
+  {
+    "revision": "8df450c84dd13217681d4dab7d84b99b",
+    "url": "/Filmoteca/static/media/zodiac.8df450c8.jpg"
+  }
+]);
